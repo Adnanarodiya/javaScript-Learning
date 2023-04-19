00b437b2600c9482e8b73dc11bf40cceb7a3e241
Adnanarodiya <aiarodiya07@gmail.com>
2023-04-19 16:36:22 +0530

    first commit

---
+++ Variable.js
@@ -0,0 +1,13 @@
+var fullName = 'Adnan Arodiay';
+
+var courseName = 'javaScript';
+
+var loggedIn = 'true';
+
+console.log(loggedCount);
+
+var loggedCount = 35;
+
+var paymentMode = 'credit Card';
+
+console.log(paymentMode);
---
+++ signUp.js
@@ -0,0 +1,36 @@
+const uid = 'abc123';
+
+var firstName = 'Adnan';
+
+var lastName = ' Arodiya';
+
+var email = 'aiarodiya07@gmail.com';
+
+var passsword = '123456';
+
+var confirmPasssword = '123456';
+
+var country = 'India';
+
+var state = 'Gujarat';
+
+var courseCount = '0';
+
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+console.log(uid);
+
+console.log('Full name is  : ' + firstName + lastName);
+
+console.log(email);
+
+console.log(`
+    With unique ID: ${uid}
+    User is: ${firstName}${lastName}
+    and email is : ${email}
+    and uses the password is: ${passsword}
+    and country is : ${country}
+    and state is : ${state}
+
+`);
